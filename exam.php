dd44c73ffab7f8298eec3f4d1d4d19a7601c472e
yse12456 <190167ad@yse-c.net>
2021-02-10 14:12:11 +0900

    push

---
+++ exam.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<?php
+echo '<title>Hello World</title>';
+?>
+</head>
+<body>
+<?php
+       echo '<p>My First Page</p>';
+?>
+<p>And this content is static HTML</p>
+</body>
+</html>
